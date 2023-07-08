f74293c82234d988316a8c541b6bd546cb43a636
Alex Hunsley <alex.hunsley@gmail.com>
2023-07-08 13:17:42 +0100

    Now processes a named toml file, and all does all found .toml in current dir (note: output files clobber previous ones currently, same name for all)

--- ext_highlights.py
+++ ext_highlights.py
@@ -1,112 +1,53 @@
-# ext_highlights.py
-
+import glob
+import sys
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.video.io.VideoFileClip import VideoFileClip
-# from moviepy.video.fx import crop
 from moviepy.video.fx.all import crop
 import toml
 
 
-# Load the TOML file
-with open("output_green.toml", 'r') as f:
-    data = toml.load(f)
+def process_segment(video_title, idx, segment):
+	# Convert start and end times to seconds
+	h, m, s = map(int, segment['start_time'].split(':'))
+	start_time = h*3600 + m*60 + s
 
-# Extract video details
-video_data = data['video'][0]
+	h, m, s = map(int, segment['end_time'].split(':'))
+	end_time = h*3600 + m*60 + s
 
-for idx, segment in enumerate(video_data['segments']):
+	# Load the video
+	clip = VideoFileClip(video_title)
 
-    # Convert start and end times to seconds
-    h, m, s = map(int, segment['start_time'].split(':'))
-    start_time = h*3600 + m*60 + s
+	# Trim the video
+	clip = clip.subclip(start_time, end_time)
 
-    h, m, s = map(int, segment['end_time'].split(':'))
-    end_time = h*3600 + m*60 + s
+	# Crop the video
+	rect = segment['clip_rect']
+	clip = clip.fx(crop, x1=rect['x'], y1=rect['y'], x2=rect['end_x'], y2=rect['end_y'])
 
-    # Load the video
-    clip = VideoFileClip(video_data['title'])
+	# Save the segment as a separate file
+	clip.write_videofile(f"output_{idx}.mp4")
 
-    # Trim the video
-    clip = clip.subclip(start_time, end_time)
+def process_video_toml(toml_file):
+	# Load the TOML file
+	with open(toml_file, 'r') as f:
+		data = toml.load(f)
 
-    # Crop the video
-    rect = segment['clip_rect']
-    clip = clip.fx(crop, x1=rect['x'], y1=rect['y'], x2=rect['end_x'], y2=rect['end_y'])
-    # clip = clip.fx(crop, x_start=rect['x'], y_start=rect['y'], 
-    #                width=rect['end_x']-rect['x'], height=rect['end_y']-rect['y'])
-    
-    # Save the segment as a separate file
-    clip.write_videofile(f"output_{idx}.mp4")
+	# Extract video details
+	video_data = data['video'][0]
 
+	for idx, segment in enumerate(video_data['segments']):
+		process_segment(video_data['title'], idx, segment)
 
+if __name__ == "__main__":
+	# Check if the script was called with an argument
+	if len(sys.argv) != 2:
+		sys.exit("Error: This script requires one argument: the name of the TOML file to process, or 'all'.")
 
-
-
-# # Load the TOML file
-# with open("output_green.toml", 'r') as f:
-# 	data = toml.load(f)
-
-# # Extract video details
-# video_data = data['video'][0]
-
-# for idx, segment in enumerate(video_data['segments']):
-
-# 	print(f" --------------- idx {idx} segment {segment}")
-
-# 	# Convert start and end times to seconds
-# 	h, m, s = map(int, segment['start_time'].split(':'))
-# 	start_time = h*3600 + m*60 + s
-
-# 	h, m, s = map(int, segment['end_time'].split(':'))
-# 	end_time = h*3600 + m*60 + s
-
-# 	print(f"video_data = {video_data}")
-
-# 	# Load the video
-# 	clip = VideoFileClip(video_data['title'])
-
-# 	# Trim the video
-# 	clip = clip.subclip(start_time, end_time)
-
-# 	# Crop the video
-# 	rect = segment['clip_rect']
-# 	clip = clip.crop(x_start=rect['x'], y_start=rect['y'], 
-# 					 width=rect['end_x']-rect['x'], height=rect['end_y']-rect['y'])
-	
-# 	# Save the segment as a separate file
-# 	clip.write_videofile(f"output_{idx}.mp4")
-
-
-
-# # Load the TOML file
-# with open("output_green.toml", 'r') as f:
-# 	data = toml.load(f)
-
-# # Extract video details
-# video_data = data.get('video')
-
-# for idx, segment in enumerate(video_data.get('segments')):
-
-# 	print(f"idx {idx} segment {segment}")
-
-# 	# Convert start and end times to seconds
-# 	h, m, s = map(int, segment['start_time'].split(':'))
-# 	start_time = h*3600 + m*60 + s
-
-# 	h, m, s = map(int, segment['end_time'].split(':'))
-# 	end_time = h*3600 + m*60 + s
-
-# 	# Load the video
-# 	clip = VideoFileClip(video_data['title'])
-
-# 	# Trim the video
-# 	clip = clip.subclip(start_time, end_time)
-
-# 	# Crop the video
-# 	rect = segment['clip_rect']
-# 	clip = clip.crop(x_start=rect['x'], y_start=rect['y'], 
-# 					 x_end=rect['end_x'], y_end=rect['end_y'])
-	
-# 	# Save the segment as a separate file
-# 	clip.write_videofile(f"output_{idx}.mp4")
-
+	# If the argument is 'all', process all .toml files in the current directory
+	if sys.argv[1] == 'all':
+		toml_files = glob.glob("*.toml")
+		for toml_file in toml_files:
+			process_video_toml(toml_file)
+	# Otherwise, process the specified TOML file
+	else:
+		process_video_toml(sys.argv[1])
