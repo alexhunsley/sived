4e2c181334623c09c63e9e76123e7e081e703f3f
Alex Hunsley <alex.hunsley@gmail.com>
2023-07-09 11:28:02 +0100

    Convert py file to spaces indentation, since gpt keeps going back to that.

--- ext_highlights.py
+++ ext_highlights.py
@@ -29,67 +29,67 @@ def time_to_seconds(time_string):
 
 
 def process_segment(video_path, idx, desc, segment):
-	# filename without extension
-	base_name = os.path.splitext(video_path)[0]
-	output_filename = f"{base_name}__seg{idx:04d}__{desc}.mp4"
+    # filename without extension
+    base_name = os.path.splitext(video_path)[0]
+    output_filename = f"{base_name}__seg{idx:04d}__{desc}.mp4"
 
-	if os.path.isfile(output_filename) and not force_overwrite_existing:
-		print(f"File {output_filename} already exists, skipping...")
-		return
+    if os.path.isfile(output_filename) and not force_overwrite_existing:
+        print(f"File {output_filename} already exists, skipping...")
+        return
 
-	start_time = time_to_seconds(segment['start_time'])
-	end_time = time_to_seconds(segment['end_time'])
+    start_time = time_to_seconds(segment['start_time'])
+    end_time = time_to_seconds(segment['end_time'])
 
-	clip = VideoFileClip(video_path)
+    clip = VideoFileClip(video_path)
 
-	# Trim
-	clip = clip.subclip(start_time, end_time)
-	clip_duration = clip.duration  # keep track of the duration
+    # Trim
+    clip = clip.subclip(start_time, end_time)
+    clip_duration = clip.duration  # keep track of the duration
 
-	# print(f"======= Duration found: {clip_duration}")
+    # print(f"======= Duration found: {clip_duration}")
 
-	# Crop the video if clip_rect is specified
-	if 'clip_rect' in segment:
-		rect = segment['clip_rect']
-		clip = clip.fx(crop, x1=rect['x'], y1=rect['y'], x2=rect['end_x'], y2=rect['end_y'])
+    # Crop the video if clip_rect is specified
+    if 'clip_rect' in segment:
+        rect = segment['clip_rect']
+        clip = clip.fx(crop, x1=rect['x'], y1=rect['y'], x2=rect['end_x'], y2=rect['end_y'])
 
-	# Add image overlay if watermark_filename is specified
-	if watermark_filename is not None:
-		# Load the image and resize it
-		img = ImageClip(watermark_filename)
-		img = img.fx(resize, height=watermark_height)
+    # Add image overlay if watermark_filename is specified
+    if watermark_filename is not None:
+        # Load the image and resize it
+        img = ImageClip(watermark_filename)
+        img = img.fx(resize, height=watermark_height)
 
-		# Set the image clip's duration to match the video clip's
-		img = img.set_duration(clip_duration)
+        # Set the image clip's duration to match the video clip's
+        img = img.set_duration(clip_duration)
 
-		# composite video clip with the watermark image overlay
-		clip = CompositeVideoClip([clip, img.set_position(default_watermark_position)])
+        # composite video clip with the watermark image overlay
+        clip = CompositeVideoClip([clip, img.set_position(default_watermark_position)])
 
-	clip.write_videofile(output_filename)
+    clip.write_videofile(output_filename)
 
 
 def process_video_toml(toml_file):
-	with open(toml_file, 'r') as f:
-		data = toml.load(f)
+    with open(toml_file, 'r') as f:
+        data = toml.load(f)
 
-	video_data = data['video'][0]
+    video_data = data['video'][0]
 
-	print(f"\n\n======= Processing video: {os.path.basename(video_data['title'])} =======\n")
+    print(f"\n\n======= Processing video: {os.path.basename(video_data['title'])} =======\n")
 
-	for idx, segment in enumerate(video_data['segments']):
-		process_segment(video_data['title'], idx, segment['desc'], segment)
+    for idx, segment in enumerate(video_data['segments']):
+        process_segment(video_data['title'], idx, segment['desc'], segment)
 
 
 if __name__ == "__main__":
-	# Check if the script was called with an argument
-	if len(sys.argv) != 2:
-		sys.exit("Error: This script requires one argument: the name of the TOML file to process, or 'all'.")
+    # Check if the script was called with an argument
+    if len(sys.argv) != 2:
+        sys.exit("Error: This script requires one argument: the name of the TOML file to process, or 'all'.")
 
-	# If the argument is 'all', process all .toml files in the current directory
-	if sys.argv[1] == 'all':
-		toml_files = glob.glob("*.toml")
-		for toml_file in toml_files:
-			process_video_toml(toml_file)
-	# Otherwise, process the specified TOML file
-	else:
-		process_video_toml(sys.argv[1])
+    # If the argument is 'all', process all .toml files in the current directory
+    if sys.argv[1] == 'all':
+        toml_files = glob.glob("*.toml")
+        for toml_file in toml_files:
+            process_video_toml(toml_file)
+    # Otherwise, process the specified TOML file
+    else:
+        process_video_toml(sys.argv[1])
