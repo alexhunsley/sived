b2e03bfe1ef2f3ecb752518d059ebe13a9fcbfa2
Alex Hunsley <alex.hunsley@gmail.com>
2023-07-14 21:50:32 +0100

    Update Rect, Size

--- rect.py
+++ rect.py
@@ -2,7 +2,56 @@
 
 
 class Rect:
-    # ... existing methods here ...
+
+    def __init__(self, x, y, end_x, end_y):
+        self.x = float(x)
+        self.y = float(y)
+        self.end_x = float(end_x)
+        self.end_y = float(end_y)
+
+
+    def width(self):
+        return self.end_x - self.x
+
+
+    def height(self):
+        return self.end_y - self.y
+
+
+    def centre(self):
+        centre_x = self.x + self.width() / 2
+        centre_y = self.y + self.height() / 2
+        return centre_x, centre_y
+
+
+    def aspect_ratio(self):
+        return self.width() / self.height()
+
+
+    def match_centre(self, other_rect):
+        cx, cy = other_rect.centre()
+        half_width = self.width() / 2
+        half_height = self.height() / 2
+        self.x = cx - half_width
+        self.y = cy - half_height
+        self.end_x = cx + half_width
+        self.end_y = cy + half_height
+
+
+    def match_size(self, other_rect):
+        self.end_x = self.x + other_rect.width()
+        self.end_y = self.y + other_rect.height()
+
+
+    def match_size_maintaining_centre(self, other_rect):
+        cx, cy = self.centre()
+        half_width = other_rect.width() / 2
+        half_height = other_rect.height() / 2
+        self.x = cx - half_width
+        self.y = cy - half_height
+        self.end_x = cx + half_width
+        self.end_y = cy + half_height
+
 
     def move_minimally_to_lie_inside(self, other_rect):
         if self.width() > other_rect.width() or self.height() > other_rect.height():
@@ -20,4 +69,8 @@ class Rect:
 
         self.end_x = self.x + self.width()
         self.end_y = self.y + self.height()
+
+
+    def __str__(self):
+        return f"Rect({self.x}, {self.y}, {self.end_x}, {self.end_y})"
 
--- size.py
+++ size.py
@@ -1,23 +1,44 @@
 # size.py
 
+def clip(value, lower, upper):
+    return lower if value < lower else upper if value > upper else value
+
+
+def min_ratio(a, b):
+	return min(a, b) if a > 1 else max(a, b)
+
+
+def max_ratio(a, b):
+	return max(a, b) if a > 1 else min(a, b)
+
+
 class Size:
     def __init__(self, width, height):
         self.width = float(width)
         self.height = float(height)
 
+
     def aspect_ratio(self):
         return self.width / self.height
+
 
     def scale(self, factor):
         self.width *= factor
         self.height *= factor
         return self
 
-    def min_scaled_size_that_aspect_fits(self, other_size):
+
+    def scaled(factor):
+    	return Size(self.width * scale_factor, self.height * scale_factor)
+
+
+    def scaled_to(self, other_size, ratio_choose_func, z_min=None, z_max=None):
         width_ratio = other_size.width / self.width
         height_ratio = other_size.height / self.height
-        scale_factor = max(width_ratio, height_ratio)
-        return Size(self.width * scale_factor, self.height * scale_factor)
+        scale_factor = ratio_choose_func(width_ratio, height_ratio)
+        scale_factor = clip(scale_factor, z_min, z_max)
+        return self.scaled(scale_factor)
+
 
     def __str__(self):
         return f"Size({self.width}, {self.height})"
