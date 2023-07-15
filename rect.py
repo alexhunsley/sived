c63ccb1c294d3972927acc949b503afe7a44c14b
Alex Hunsley <alex.hunsley@gmail.com>
2023-07-15 20:16:32 +0100

    Changes to rect

--- rect.py
+++ rect.py
@@ -1,71 +1,111 @@
 # rect.py
 
+import functools
 from dataclasses import dataclass
 
+@functools.total_ordering
 @dataclass(frozen=True, order=True)
 class Rect:
+    x: float
+    y: float
+    size: Size
+    end_x: float
+    end_y: float
+    centre_x: float
+    centre_y: float 
 
 
-    def __init__(self, x, y, end_x, end_y):
-        self.x = float(x)
-        self.y = float(y)
-        self.end_x = float(end_x)
-        self.end_y = float(end_y)
-        self.aspect_ratio = self.width() / self.height() if self.height() != 0 else None
-        self.centre = (self.x + self.width() / 2, self.y + self.height() / 2)
-        self.width = self.end_x - self.x
-        self.height = self.end_y - self.y
+    # Call make with either of these styles:
+    #
+    #    Rect.make(x, y, size)
+    #    Rect.make(x, y, end_x, end_y)
+    @classmethod
+    def make(cls, x, y, arg3, arg4=None):
+    	if arg4:
+    		return Rect.make_with_end_coords(x, y, arg3, arg4)
+
+    	return Rect.make_with_size(x, y, arg3)
 
 
-    def width(self):
-        return self.end_x - self.x
+    @classmethod
+    def make_with_end_coords(cls, x, y, end_x, end_y):
+    	return Rect(
+    		x,
+    		y,
+    		Size(end_x - x, end_y - y),
+			end_x,
+			end_y,
+			(x + end_x) / 2.0,
+			(y + end_y) / 2.0,
+		)
 
 
-    def height(self):
-        return self.end_y - self.y
+    @classmethod
+    def make_with_size(cls, x, y, size):
+    	return Rect(
+    		x,
+    		y,
+    		size,
+			x + size.width,
+			y + size.height,
+			x + size.width / 2.0,
+			y + size.height / 2.0
+		)
 
 
-    def match_centre(self, other_rect):
-        cx, cy = other_rect.centre()
-        half_width = self.width() / 2
-        half_height = self.height() / 2
-        self.x = cx - half_width
-        self.y = cy - half_height
-        self.end_x = cx + half_width
-        self.end_y = cy + half_height
+    def __lt__(self, other: 'Size') -> bool:
+        return self.width < other.width and self.height < other.height
 
 
-    def match_size(self, other_rect):
-        self.end_x = self.x + other_rect.width()
-        self.end_y = self.y + other_rect.height()
+    def __eq__(self, other: 'Size') -> bool:
+        return self.width == other.width and self.height == other.height
+
+
+    def matched_centre(self, other_rect):
+    	return Rect.make_with_size(
+    		other_rect.centre_x - self.size.width / 2.0,
+			other_rect.centre_y - self.y,
+			self.centre
+		)
+
+
+    def matched_size(self, other_rect):
+    	return Rect.make_with_size(
+    		self.x,
+    		self.y,
+    		other_rect.size
+		)
 
 
     def match_size_maintaining_centre(self, other_rect):
-        cx, cy = self.centre()
-        half_width = other_rect.width() / 2
-        half_height = other_rect.height() / 2
-        self.x = cx - half_width
-        self.y = cy - half_height
-        self.end_x = cx + half_width
-        self.end_y = cy + half_height
+    	return Rect.make_with_size(
+    		self.x - other_rect.size.x / 2,
+    		self.y - other_rect.size.y / 2,
+    		other_rect.size
+		)
 
 
     def moved_minimally_to_lie_inside(self, other_rect):
-        if self.width() > other_rect.width() or self.height() > other_rect.height():
+    	# we check `not for self.size < other.size` rather than `self.size > other.size`
+    	# since inequality check on sizes demands that both width and height have the in equality hold
+    	# (and we need to know if even just one of width or height fails the check).
+        if not self.size < other_rect.size:
             raise ValueError("Receiver rectangle is larger than the other rectangle.")
         
+        new_x = self.x
+        new_y = self.y
+
         if self.x < other_rect.x:
-            self.x = other_rect.x
+            new_x = other_rect.x
         elif self.end_x > other_rect.end_x:
-            self.x -= (self.end_x - other_rect.end_x)
+            new_x -= (self.end_x - other_rect.end_x)
 
         if self.y < other_rect.y:
-            self.y = other_rect.y
+            new_y = other_rect.y
         elif self.end_y > other_rect.end_y:
-            self.y -= (self.end_y > other_rect.end_y)
+            new_y -= (self.end_y > other_rect.end_y)
 
-        self.end_x = self.x + self.width()
-        self.end_y = self.y + self.height()
+       return Size.make_with_size(new_x, new_y, self.size)
 
 
     def __str__(self):
