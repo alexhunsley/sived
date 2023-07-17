a6fb84655b63bedb2f163fe025f1b575f2a5a272
Alex Hunsley <alex.hunsley@gmail.com>
2023-07-17 21:00:15 +0100

    Tidy, edit test toml

--- helpers.py
+++ helpers.py
@@ -3,12 +3,9 @@
 import os
 import re
 
-from .ext_highlights import working_dir
-
 
 def dbg(string):
-    pass
-    # print(string)
+    print(f"------------------------------ {string}")
 
 
 def p(string=""):
@@ -24,9 +21,13 @@ def make_abs_path_to_script_dir(rel_path):
     return os.path.normpath(os.path.join(script_dir, rel_path))
 
 
-def make_abs_path_rel_to_working_dir(rel_path):
-    return os.path.normpath(os.path.join(working_dir, rel_path))
-
 
 def sanitize_filename(filename):
     return re.sub(r'(?u)[^-\w.{}]', '', filename.strip())
+
+
+from .ext_highlights import working_dir
+
+def make_abs_path_rel_to_working_dir(rel_path):
+    return os.path.normpath(os.path.join(working_dir, rel_path))
+
